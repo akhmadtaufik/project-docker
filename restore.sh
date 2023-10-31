1f221cfef08d36817d365d9342b831d72b6720a1
akhmadtaufik <akhmadtaufik1703@gmail.com>
2023-10-31 19:00:09 +0700

    Updated restore.sh to include timestamps in log file and entries

--- restore.sh
+++ restore.sh
@@ -6,10 +6,13 @@ DB_PASSWORD=$POSTGRES_PASSWORD
 DB_NAME="todo-db"
 CONTAINER_NAME="todo-db-container"
 BACKUP_DIR="/backups"
-LOG_FILE="/home/phy/LinuxAndContainer/web-app/logs/restore.log"
+
+# Membuat timestamp
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+LOG_FILE="/home/phy/LinuxAndContainer/web-app/logs/restore_$TIMESTAMP.log"
 
 # Menampilkan daftar file backup
-echo "Daftar file backup:" | tee -a $LOG_FILE
+echo "$(date +"%Y-%m-%d %H:%M:%S") Daftar file backup:" | tee -a $LOG_FILE
 docker exec $CONTAINER_NAME ls $BACKUP_DIR | tee -a $LOG_FILE
 
 # Meminta pengguna untuk memasukkan nama file backup
@@ -20,15 +23,15 @@ BACKUP_PATH="$BACKUP_DIR/$BACKUP_FILE"
 
 # Cek apakah file backup ada
 if docker exec $CONTAINER_NAME test -f $BACKUP_PATH; then
-    echo "File backup ditemukan." | tee -a $LOG_FILE
+    echo "$(date +"%Y-%m-%d %H:%M:%S") File backup ditemukan." | tee -a $LOG_FILE
 else
-    echo "File backup tidak ditemukan. Proses restore dibatalkan." | tee -a $LOG_FILE >&2
+    echo "$(date +"%Y-%m-%d %H:%M:%S") File backup tidak ditemukan. Proses restore dibatalkan." | tee -a $LOG_FILE >&2
     exit 1
 fi
 
 # Command untuk merestore database
 if docker exec -i $CONTAINER_NAME bash -c "psql -U $DB_USER -d $DB_NAME < $BACKUP_PATH"; then
-    echo "Restore database berhasil." | tee -a $LOG_FILE
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Restore database berhasil." | tee -a $LOG_FILE
 else
-    echo "Restore database gagal." | tee -a $LOG_FILE >&2
+    echo "$(date +"%Y-%m-%d %H:%M:%S") Restore database gagal." | tee -a $LOG_FILE >&2
 fi
